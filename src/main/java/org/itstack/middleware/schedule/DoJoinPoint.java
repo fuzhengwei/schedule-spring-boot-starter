9f8208610d4d99d48a31d3b783406067ae8a027b
fuzhengwei <184172133@qq.com>
2019-12-07 02:25:02 +0800

    init

--- src/main/java/org/itstack/middleware/schedule/DoJoinPoint.java
+++ src/main/java/org/itstack/middleware/schedule/DoJoinPoint.java
@@ -9,7 +9,6 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
